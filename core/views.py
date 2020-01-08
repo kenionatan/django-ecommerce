d2d5d8091d9c5193d5d15b89e36cfc955f03676b
Kenio Natan <kenionatan@gmail.com>
2020-01-08 09:41:29 -0200

    Adding .gitattributes to ignore static files for a better language classification

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from .models import Item
+from .models import Item, OrderItem, Order
 
 
 def products(request):
@@ -22,3 +22,18 @@ class HomeView(ListView):
 class ItemDetailView(DetailView):
     model = Item
     template_name = "product.html"
+
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item = OrderItem.objects.create(item=item)
+    order_qs = Order.objects.filter(user=request, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # Check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+    else:
+        order = Order.objects.create(user=request.user)
+        order.items.add(order_item)
