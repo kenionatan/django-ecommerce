5b5ad59aadfc03b10d0fdb089580fb1f71586127
Kenio Natan <kenionatan@gmail.com>
2020-02-01 09:32:08 -0200

    Adding new item to cart

--- core/views.py
+++ core/views.py
@@ -28,7 +28,7 @@ class ItemDetailView(DetailView):
 
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
-    order_item = OrderItem.objects.create(item=item)
+    order_item = OrderItem.objects.get_or_create(item=item)
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     if order_qs.exists():
         order = order_qs[0]
@@ -36,6 +36,8 @@ def add_to_cart(request, slug):
         if order.items.filter(item__slug=item.slug).exists():
             order_item.quantity += 1
             order_item.save()
+        else:
+            order.items.add(order_item)
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(
